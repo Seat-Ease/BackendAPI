345d5dd23956cefadaaa61e9ad6e2b363e004acf
aircongo <natelufuluabo@yahoo.ca>
2025-03-08 15:14:44 -0500

    updated restaurants model

--- models/Restaurants.js
+++ models/Restaurants.js
@@ -5,7 +5,11 @@ const restaurantSchema = new Schema({
     nom : String,
     courriel : String,
     adresse : String,
-    telephone : String
+    telephone : String,
+    disponibilites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Disponibilite' }],
+    reservations: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reservation' }],
+    employes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Employe' }],
+    tables: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Table' }]
 })
 
 const Restaurant = mongoose.model('Restaurant', restaurantSchema)
