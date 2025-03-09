96f93e20fe23608e93566e000424eba3ee6f402e
aircongo <natelufuluabo@yahoo.ca>
2025-03-09 17:40:37 -0400

    added logout function in /auth route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const AuthService = require('../services/authService')
 
 router.post('/login', AuthService.login)
-router.post('/logout', )
+router.post('/logout', AuthService.logout)
 
 module.exports = router
